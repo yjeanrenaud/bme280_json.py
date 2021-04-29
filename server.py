bd2ee5c540a339ad99cde72b80c01c9a19261428
Yves Jeanrenaud <yjeanrenaud@users.noreply.github.com>
2021-04-29 16:01:10 +0200

    added some basic
    my kid pulled the i2c jumper wires accidentally, so the server failed to launch dragging along homebridge into the abyss, so I added some basic checks for that, too.

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python
-# put togetherthe work of Matt Hawkins (21/01/2018) https://www.raspberrypi-spy.co.uk/2016/07/using-bme280-i2c-temperature-pressure-sensor-in-python/ and Matteo Abrate
+#!/usr/bin/env python2
+# put together the work of Matt Hawkins (21/01/2018) https://www.raspberrypi-spy.co.uk/2016/07/using-bme280-i2c-temperature-pressure-sensor-in-python/ and Matteo Abrate
 # https://gist.github.com/nitaku/10d0662536f37a087e1b quick and dirty by Yves Jeanrenaud
 # this script is run via #screen -dmS bme280_json python3 server.py
+# my kid pulled the i2c jumper wires accidentally, so the server failed to launch dragging along homebridge into the abyss, so I added some basic checks for that, too.
+# if all goes wrong, all values bogus which is just a hint but better than nothing. chip id and chip version though speak more clearly to you
+# todo: migrate to python3
+# todo: add more checks
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
@@ -41,7 +45,14 @@ def getUChar(data,index):
 def readBME280ID(addr=DEVICE):
   # Chip ID Register Address
   REG_ID     = 0xD0
-  (chip_id, chip_version) = bus.read_i2c_block_data(addr, REG_ID, 2)
+  try: #for at least some basic error handling
+      (chip_id, chip_version) = bus.read_i2c_block_data(addr, REG_ID, 2)
+  
+  except:
+      chip_id="BME/P280 not found"
+      chip_version="check I2C bus connection"
+      
+  
   return (chip_id, chip_version)
 
 def readBME280All(addr=DEVICE):
@@ -61,86 +72,94 @@ def readBME280All(addr=DEVICE):
 
   # Oversample setting for humidity register - page 26
   OVERSAMPLE_HUM = 2
-  bus.write_byte_data(addr, REG_CONTROL_HUM, OVERSAMPLE_HUM)
+  try: #for at least some basic error handling
+    bus.write_byte_data(addr, REG_CONTROL_HUM, OVERSAMPLE_HUM)
 
-  control = OVERSAMPLE_TEMP<<5 | OVERSAMPLE_PRES<<2 | MODE
-  bus.write_byte_data(addr, REG_CONTROL, control)
+    control = OVERSAMPLE_TEMP<<5 | OVERSAMPLE_PRES<<2 | MODE
+    bus.write_byte_data(addr, REG_CONTROL, control)
 
-  # Read blocks of calibration data from EEPROM
-  # See Page 22 data sheet
-  cal1 = bus.read_i2c_block_data(addr, 0x88, 24)
-  cal2 = bus.read_i2c_block_data(addr, 0xA1, 1)
-  cal3 = bus.read_i2c_block_data(addr, 0xE1, 7)
+    # Read blocks of calibration data from EEPROM
+    # See Page 22 data sheet
+    cal1 = bus.read_i2c_block_data(addr, 0x88, 24)
+    cal2 = bus.read_i2c_block_data(addr, 0xA1, 1)
+    cal3 = bus.read_i2c_block_data(addr, 0xE1, 7)
 
-  # Convert byte data to word values
-  dig_T1 = getUShort(cal1, 0)
-  dig_T2 = getShort(cal1, 2)
-  dig_T3 = getShort(cal1, 4)
+    # Convert byte data to word values
+    dig_T1 = getUShort(cal1, 0)
+    dig_T2 = getShort(cal1, 2)
+    dig_T3 = getShort(cal1, 4)
 
-  dig_P1 = getUShort(cal1, 6)
-  dig_P2 = getShort(cal1, 8)
-  dig_P3 = getShort(cal1, 10)
-  dig_P4 = getShort(cal1, 12)
-  dig_P5 = getShort(cal1, 14)
-  dig_P6 = getShort(cal1, 16)
-  dig_P7 = getShort(cal1, 18)
-  dig_P8 = getShort(cal1, 20)
-  dig_P9 = getShort(cal1, 22)
+    dig_P1 = getUShort(cal1, 6)
+    dig_P2 = getShort(cal1, 8)
+    dig_P3 = getShort(cal1, 10)
+    dig_P4 = getShort(cal1, 12)
+    dig_P5 = getShort(cal1, 14)
+    dig_P6 = getShort(cal1, 16)
+    dig_P7 = getShort(cal1, 18)
+    dig_P8 = getShort(cal1, 20)
+    dig_P9 = getShort(cal1, 22)
 
-  dig_H1 = getUChar(cal2, 0)
-  dig_H2 = getShort(cal3, 0)
-  dig_H3 = getUChar(cal3, 2)
+    dig_H1 = getUChar(cal2, 0)
+    dig_H2 = getShort(cal3, 0)
+    dig_H3 = getUChar(cal3, 2)
 
-  dig_H4 = getChar(cal3, 3)
-  dig_H4 = (dig_H4 << 24) >> 20
-  dig_H4 = dig_H4 | (getChar(cal3, 4) & 0x0F)
+    dig_H4 = getChar(cal3, 3)
+    dig_H4 = (dig_H4 << 24) >> 20
+    dig_H4 = dig_H4 | (getChar(cal3, 4) & 0x0F)
 
-  dig_H5 = getChar(cal3, 5)
-  dig_H5 = (dig_H5 << 24) >> 20
-  dig_H5 = dig_H5 | (getUChar(cal3, 4) >> 4 & 0x0F)
+    dig_H5 = getChar(cal3, 5)
+    dig_H5 = (dig_H5 << 24) >> 20
+    dig_H5 = dig_H5 | (getUChar(cal3, 4) >> 4 & 0x0F)
 
-  dig_H6 = getChar(cal3, 6)
+    dig_H6 = getChar(cal3, 6)
 
-  # Wait in ms (Datasheet Appendix B: Measurement time and current calculation)
-  wait_time = 1.25 + (2.3 * OVERSAMPLE_TEMP) + ((2.3 * OVERSAMPLE_PRES) + 0.575) + ((2.3 * OVERSAMPLE_HUM)+0.575)
-  time.sleep(wait_time/1000)  # Wait the required time  
+    # Wait in ms (Datasheet Appendix B: Measurement time and current calculation)
+    wait_time = 1.25 + (2.3 * OVERSAMPLE_TEMP) + ((2.3 * OVERSAMPLE_PRES) + 0.575) + ((2.3 * OVERSAMPLE_HUM)+0.575)
+    time.sleep(wait_time/1000)  # Wait the required time  
 
-  # Read temperature/pressure/humidity
-  data = bus.read_i2c_block_data(addr, REG_DATA, 8)
-  pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
-  temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
-  hum_raw = (data[6] << 8) | data[7]
+    # Read temperature/pressure/humidity
+    data = bus.read_i2c_block_data(addr, REG_DATA, 8)
+    pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+    temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+    hum_raw = (data[6] << 8) | data[7]
 
-  #Refine temperature
-  var1 = ((((temp_raw>>3)-(dig_T1<<1)))*(dig_T2)) >> 11
-  var2 = (((((temp_raw>>4) - (dig_T1)) * ((temp_raw>>4) - (dig_T1))) >> 12) * (dig_T3)) >> 14
-  t_fine = var1+var2
-  temperature = float(((t_fine * 5) + 128) >> 8);
+    #Refine temperature
+    var1 = ((((temp_raw>>3)-(dig_T1<<1)))*(dig_T2)) >> 11
+    var2 = (((((temp_raw>>4) - (dig_T1)) * ((temp_raw>>4) - (dig_T1))) >> 12) * (dig_T3)) >> 14
+    t_fine = var1+var2
+    temperature = float(((t_fine * 5) + 128) >> 8);
 
-  # Refine pressure and adjust for temperature
-  var1 = t_fine / 2.0 - 64000.0
-  var2 = var1 * var1 * dig_P6 / 32768.0
-  var2 = var2 + var1 * dig_P5 * 2.0
-  var2 = var2 / 4.0 + dig_P4 * 65536.0
-  var1 = (dig_P3 * var1 * var1 / 524288.0 + dig_P2 * var1) / 524288.0
-  var1 = (1.0 + var1 / 32768.0) * dig_P1
-  if var1 == 0:
-    pressure=0
-  else:
-    pressure = 1048576.0 - pres_raw
-    pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
-    var1 = dig_P9 * pressure * pressure / 2147483648.0
-    var2 = pressure * dig_P8 / 32768.0
-    pressure = pressure + (var1 + var2 + dig_P7) / 16.0
+    # Refine pressure and adjust for temperature
+    var1 = t_fine / 2.0 - 64000.0
+    var2 = var1 * var1 * dig_P6 / 32768.0
+    var2 = var2 + var1 * dig_P5 * 2.0
+    var2 = var2 / 4.0 + dig_P4 * 65536.0
+    var1 = (dig_P3 * var1 * var1 / 524288.0 + dig_P2 * var1) / 524288.0
+    var1 = (1.0 + var1 / 32768.0) * dig_P1
+    if var1 == 0:
+      pressure=0
+    else:
+      pressure = 1048576.0 - pres_raw
+      pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
+      var1 = dig_P9 * pressure * pressure / 2147483648.0
+      var2 = pressure * dig_P8 / 32768.0
+      pressure = pressure + (var1 + var2 + dig_P7) / 16.0
 
-  # Refine humidity
-  humidity = t_fine - 76800.0
-  humidity = (hum_raw - (dig_H4 * 64.0 + dig_H5 / 16384.0 * humidity)) * (dig_H2 / 65536.0 * (1.0 + dig_H6 / 67108864.0 * humidity * (1.0 + dig_H3 / 67108864.0 * humidity)))
-  humidity = humidity * (1.0 - dig_H1 * humidity / 524288.0)
-  if humidity > 100:
-    humidity = 100
-  elif humidity < 0:
-    humidity = 0
+    # Refine humidity
+    humidity = t_fine - 76800.0
+    humidity = (hum_raw - (dig_H4 * 64.0 + dig_H5 / 16384.0 * humidity)) * (dig_H2 / 65536.0 * (1.0 + dig_H6 / 67108864.0 * humidity * (1.0 + dig_H3 / 67108864.0 * humidity)))
+    humidity = humidity * (1.0 - dig_H1 * humidity / 524288.0)
+    if humidity > 100:
+      humidity = 100
+    elif humidity < 0:
+      humidity = 0
+
+  except:
+    #bogus values to draw your attention to a hardware issue
+    temperature=23280 # = Farenheit 451
+    pressure=0.00000001322 # = outer space
+    humidity=-255 # just impossible
+	
 
   return temperature/100.0,pressure/100.0,humidity
 
